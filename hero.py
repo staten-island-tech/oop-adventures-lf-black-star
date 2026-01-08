3682365528da82eded0ec6edaa42ecc2bd4ed211
Vincent <vincentz92@nycstudents.net>
2026-01-08 09:48:12 -0500

    Update hero.py

---
+++ hero.py
@@ -0,0 +1,13 @@
+class Hero:
+    def __init__(self, health, defense, damage, inventory, tokens, relics, speed, regen):
+        self.health = health
+        self.defense = defense
+        self.damage = damage
+        self.inventory = inventory
+        self.tokens = tokens
+        self.relics = relics
+        self.speed = speed
+        self.regen = regen
+    
+    def attack(self):
+        
