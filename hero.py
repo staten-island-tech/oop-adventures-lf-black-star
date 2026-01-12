a953927fcf40bbd442293044b913d8b2e6333275
Vincent <vincentz92@nycstudents.net>
2026-01-12 10:17:34 -0500

    code

--- hero.py
+++ hero.py
@@ -1,15 +1,24 @@
 class Hero:
-    def __init__(self, health, defense, damage, inventory, tokens, relics, speed, regen, weapon, chestplate, helmet, leggings, boots):
+    def __init__(self, health, damage, inventory, tokens, relics, speed, regen, weapon, chestplate, helmet, leggings, boots, ranking):
         self.health = health
-        self.defense = defense
         self.damage = damage
         self.inventory = inventory
         self.tokens = tokens
         self.relics = relics
         self.speed = speed
         self.regen = regen
+        self.weapon = weapon
+        self.chestplate = chestplate
+        self.helmet = helmet
+        self.leggings = leggings
+        self.boots = boots
+        self.ranking = ranking
+
+    def apply_boosts(self):
+        self.health += (self.chestplate + self.helmet + self.leggings + self.boots)
+        self.damage += self.weapon
     
     def attack(self):
         print("yay")
 
-adventurer = Hero(100, 0, 20, [], 0, [], 1, 0)
+adventurer = Hero(100, 20, [], 0, [], 1, 0, 0, 0, 0, 0, 0, 0)
--- shop.py
+++ shop.py
@@ -2,21 +2,26 @@ from hero import adventurer #TEMP
 import random
 
 class shop:
-    def __init__(self, name, equipment, potions):
+    def __init__(self, name, equipment):
         self.name = name
         self.equipment = equipment
-        self.potions = potions
     
     def player_buy(self):
-        prompt = input("What selection of items would you like to buy?")
-        if prompt.isdigit() == False:
-            print("Invalid input. Please try again.")
-        else:
-            prompt = int(prompt)
-        if prompt == 1:
-            print("Equipment provides an all-around boost to your health, damage and defense in battle.")
-        if prompt == 2:
-            print("Potions are one-time consumables that you can use during battle.")
+        prompt = "Not chosen!"
+        while prompt != 2:
+            prompt = input(f"This is a {self.name}. What equipment would you like to purchase?")
+            if prompt.isdigit() == False:
+                print("Invalid input. Please try again.")
+            else:
+                prompt = int(prompt)
+            if prompt == 1:
+                print("Equipment provides an all-around boost to your health, damage and defense in battle.")
+                print(f"Helmet: +{self.equipment[0]} health. Chestplate: +{self.equipment[1]} health. Leggings: +{self.equipment[2]} health. Boots: +{self.equipment[3]} health.")
+                if len(self.equipment) == 0: #make weapon
+                    print("No equipment found!")
+            if prompt == 2:
+                print(f"You left the {self.name}.")
+                return
 
-blacksmith = shop("Blacksmith", [adventurer.helmet, adventurer.chestplate, adventurer. leggings, adventurer.boots, adventurer.weapon], [], )
-shop_list = [blacksmith, ]
+blacksmith = shop("blacksmith", [random.randint((adventurer.helmet), (adventurer.helmet + 25)), random.randint((adventurer.chestplate), (adventurer.chestplate + 20)), random.randint((adventurer.leggings), (adventurer.leggings + 25)), random.randint((adventurer.boots), (adventurer.boots + 20)), random.randint((adventurer.weapon), (adventurer.weapon + 30))])
+blacksmith.player_buy()
