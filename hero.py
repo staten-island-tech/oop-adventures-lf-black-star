52639d7313596bb0cd6c47e5b4b63a812a81eb11
Vincent <vincentz92@nycstudents.net>
2026-01-14 18:20:47 -0500

    moved

--- hero.py
+++ hero.py
@@ -1,5 +1,5 @@
 class Hero:
-    def __init__(self, health, damage, inventory, tokens, relics, speed, regen, weapon, chestplate, helmet, leggings, boots, ranking):
+    def __init__(self, health, damage, inventory, tokens, relics, speed, regen, wealth, weapon, helmet, chestplate, leggings, boots, ranking):
         self.health = health
         self.damage = damage
         self.inventory = inventory
@@ -7,18 +7,20 @@ class Hero:
         self.relics = relics
         self.speed = speed
         self.regen = regen
+        self.wealth = wealth
         self.weapon = weapon
-        self.chestplate = chestplate
         self.helmet = helmet
+        self.chestplate = chestplate
         self.leggings = leggings
         self.boots = boots
         self.ranking = ranking
 
     def apply_boosts(self):
+
         self.health += (self.chestplate + self.helmet + self.leggings + self.boots)
         self.damage += self.weapon
     
     def attack(self):
         print("yay")
 
-adventurer = Hero(100, 20, [], 0, [], 1, 0, 5, 10, 5, 5, 5, 0)
+adventurer = Hero(100, 20, [], 0, [], 1, 0, 1, 5, 10, 5, 5, 5, 0)
