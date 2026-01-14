d28ba154aeaed334b9a5b891b3009b2dd77969cc
Vincent <vincentz92@nycstudents.net>
2026-01-14 18:20:31 -0500

    moving

--- equipment.py
+++ equipment.py
@@ -8,6 +8,7 @@ class Equipment:
 
 hermes = Equipment("Blessing of Hermes", adventurer.speed, 1)
 aphrodite = Equipment("Blessing of Aphrodite", adventurer.regen, (adventurer.health/10))
-midas = Equipment("Blessing of Midas", adventurer.wealth, 1)
+plutus = Equipment("Blessing of Plutus", adventurer.wealth, 1)
 athena = Equipment("Blessing of Athena", adventurer.weapon, round(adventurer.weapon/5))
 ares = Equipment("Blessing of Ares", adventurer.damage, round(adventurer.damage/2))
+relic_list = [hermes, aphrodite, ]
