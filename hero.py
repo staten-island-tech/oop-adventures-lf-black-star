4c4566ae9f523583584babc43fbdc9332a5e1492
Vincent <vincentz92@nycstudents.net>
2026-01-14 20:48:39 -0500

    a
    8:48

--- hero.py
+++ hero.py
@@ -1,10 +1,10 @@
 class Hero:
-    def __init__(self, health, damage, inventory, tokens, relics, speed, regen, wealth, weapon, helmet, chestplate, leggings, boots, ranking):
+    def __init__(self, health, damage, inventory, tokens, obtained_relics, speed, regen, wealth, weapon, helmet, chestplate, leggings, boots, ranking):
         self.health = health
         self.damage = damage
         self.inventory = inventory
         self.tokens = tokens
-        self.relics = relics
+        self.obtained_relics = obtained_relics
         self.speed = speed
         self.regen = regen
         self.wealth = wealth
@@ -16,11 +16,20 @@ class Hero:
         self.ranking = ranking
 
     def apply_boosts(self):
-
+        if hermes in self.obtained_relics:
+            self.speed += hermes.stat_boost
+        if aphrodite in self.obtained_relics:
+            self.regen += aphrodite.stat_boost
+        if plutus in self.obtained_relics:
+            self.wealth += plutus.stat_boost
+        if athena in self.obtained_relics:
+            self.weapon += athena.stat_boost
+        if ares in self.obtained_relics:
+            self.damage += ares.stat_boost
         self.health += (self.chestplate + self.helmet + self.leggings + self.boots)
         self.damage += self.weapon
     
     def attack(self):
         print("yay")
 
-adventurer = Hero(100, 20, [], 0, [], 1, 0, 1, 5, 10, 5, 5, 5, 0)
+adventurer = Hero(100, 20, [], 999999999, [], 1, 0, 1, 5, 10, 5, 5, 5, 0)
--- shop.py
+++ shop.py
@@ -9,105 +9,76 @@ class shop:
     def prepare_shop(self):
         if self.name == "blacksmith":
             global blacksmith_items
-            blacksmith_items = [{
-        "name": "Helmet",
-        "stat boost": random.randint((adventurer.ranking), (adventurer.ranking + 25)),
-        "price": (random.randint((adventurer.ranking + 10), (adventurer.ranking + 15)) * 2)}
-    ,{
-        "name": "Chestplate",
-        "stat boost": random.randint((adventurer.ranking), (adventurer.ranking + 20)),
-        "price": (random.randint((adventurer.ranking + 15), (adventurer.ranking + 20)) * 2)}
-    ,{
-        "name": "Leggings",
-        "stat boost": random.randint((adventurer.ranking), (adventurer.ranking + 25)),
-        "price": (random.randint((adventurer.ranking + 10), (adventurer.ranking + 15)) * 2)}
-    ,{
-        "name": "Boots",
-        "stat boost": random.randint((adventurer.ranking), (adventurer.ranking + 20)),
-        "price": (random.randint((adventurer.ranking + 5), (adventurer.ranking + 10)) * 2)}
-    ,{
-        "name": "Sword",
-        "stat boost": random.randint((adventurer.ranking), (adventurer.ranking + 30)),
-        "price": (random.randint((adventurer.ranking + 15), (adventurer.ranking + 20)) * 2)}]
+            blacksmith_items = [{"name": "Helmet", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 25)), "price": (random.randint((adventurer.ranking + 10), (adventurer.ranking + 15)) * 2)}
+    ,{"name": "Chestplate", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 20)), "price": (random.randint((adventurer.ranking + 15), (adventurer.ranking + 20)) * 2)}
+    ,{"name": "Leggings", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 25)), "price": (random.randint((adventurer.ranking + 10), (adventurer.ranking + 15)) * 2)}
+    ,{"name": "Boots", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 20)), "price": (random.randint((adventurer.ranking + 5), (adventurer.ranking + 10)) * 2)}
+    ,{"name": "Sword", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 30)), "price": (random.randint((adventurer.ranking + 15), (adventurer.ranking + 20)) * 2)}]
             blacksmith.player_interact()
     
     def player_interact(self):
-        item_names = [item["name"] for item in blacksmith_items]
-        prompt = "Not chosen!"
+        prompt = "Not decided."
         while prompt != 3:
+            item_names = [item["name"] for item in blacksmith_items]
+            confirm = "Not decided."
             print("1: View items")
             print("2: Sell items")
             print("3: Leave shop")
             prompt = input(f"This is a {self.name}. What would you like to do?")
-            if prompt.isdigit() == False:
+            if not prompt.isdigit():
                 print("Invalid input. Please try again.")
-            else:
-                prompt = int(prompt)
-                if prompt == 1 and len(item_names) > 0:
-                    print("Equipment provides an overall boost to your health, damage and defense in battle.")
-                    for index, item in enumerate(blacksmith_items, start = 1):
-                        print(index, ":", item)
-                    selection = input(f"What would you like to purchase?")
-                    if selection in item_names:
-                        if selection == 1:
-                            if adventurer.tokens >= blacksmith_items[0]["price"]:
-                                adventurer.tokens -= blacksmith_items[0]["price"]
-                                print(f"Purchase successful! Current token balance: {adventurer.tokens}")
-                            else:
-                                print("You do not have enough tokens.")
-                        if selection == 2:
-                            if adventurer.tokens >= blacksmith_items[1]["price"]:
-                                adventurer.tokens -= blacksmith_items[1]["price"]
-                                print(f"Purchase successful! Current token balance: {adventurer.tokens}")
-                            else:
-                                print("You do not have enough tokens.")
-                        if selection == 3:
-                            if adventurer.tokens >= blacksmith_items[2]["price"]:
-                                adventurer.tokens -= blacksmith_items[2]["price"]
-                                print(f"Purchase successful! Current token balance: {adventurer.tokens}")
-                            else:
-                                print("You do not have enough tokens.")
-                        if selection == 4:
-                            if adventurer.tokens >= blacksmith_items[3]["price"]:
-                                adventurer.tokens -= blacksmith_items[3]["price"]
-                                print(f"Purchase successful! Current token balance: {adventurer.tokens}")
-                            else:
-                                print("You do not have enough tokens.")
-                        if selection == 5:
-                            if adventurer.tokens >= blacksmith_items[4]["price"]:
-                                adventurer.tokens -= blacksmith_items[4]["price"]
-                                print(f"Purchase successful! Current token balance: {adventurer.tokens}")
-                            else:
-                                print("You do not have enough tokens.")
-                        else:
-                            print("Invalid input. Please try again.")
-                else:
-                    print("No items found!")
-                if prompt == 2:
-                    print("soon")
-                if prompt == 3:
-                    print(f"You left the {self.name}.")
-                    return "player left"
+                continue
+            prompt = int(prompt)
+            if prompt == 1:
+                for index, i in enumerate(blacksmith_items, start = 1):
+                    print(index, ":", i)
+                selection = input(f"What would you like to purchase?")
+                if not selection.isdigit():
+                    print("Invalid input. Please try again.")
+                    continue
+                item = int(selection) - 1
+                if adventurer.tokens < item["price"]:
+                      print("You do not have enough tokens.")
+                      continue
+                adventurer.tokens -= item["price"]
+                print(f"Purchase successful! Current token balance: {adventurer.tokens}")
+                while confirm == "yes" or confirm == "y" or confirm == "no" or confirm == "n":
+                      confirm = input("Would you like to equip your new item?")
+                      confirm = confirm.lower()
+                      if confirm == "y" or "yes":
+                            adventurer #later
+                      if confirm == "n" or "no":
+                            print("Item added to inventory.")
 
-blacksmith_items = [{
-        "name": "Helmet",
-        "boost": random.randint((adventurer.ranking), (adventurer.ranking + 25)),
-        "price": (random.randint((adventurer.ranking + 10), (adventurer.ranking + 15)) * 2)}
-    ,{
-        "name": "Chestplate",
-        "boost": random.randint((adventurer.ranking), (adventurer.ranking + 20)),
-        "price": (random.randint((adventurer.ranking + 15), (adventurer.ranking + 20)) * 2)}
-    ,{
-        "name": "Leggings",
-        "boost": random.randint((adventurer.ranking), (adventurer.ranking + 25)),
-        "price": (random.randint((adventurer.ranking + 10), (adventurer.ranking + 15)) * 2)}
-    ,{
-        "name": "Boots",
-        "boost": random.randint((adventurer.ranking), (adventurer.ranking + 20)),
-        "price": (random.randint((adventurer.ranking + 5), (adventurer.ranking + 10)) * 2)}
-    ,{
-        "name": "Sword",
-        "boost": random.randint((adventurer.ranking), (adventurer.ranking + 30)),
-        "price": (random.randint((adventurer.ranking + 15), (adventurer.ranking + 20)) * 2)}]
+                
+            else:
+                print("Invalid input. Please try again.")
+            if len(item_names) <= 0:
+                print("No items found!")
+            elif prompt == 2:
+                if len(adventurer.inventory) <= 0:
+                    print("Nothing is in your inventory.")
+                    continue
+                for index, i in enumerate(adventurer.inventory, start = 1):
+                    print(index, ":", i)
+                    sell_choice = input("What item would you like to sell?")
+                    if not sell_choice.isdigit():
+                        print("Invalid input. Please try again.")
+                        continue
+                    sell_choice = int(sell_choice) - 1
+            elif prompt == 3:
+                print(f"You left the {self.name}.")
+                #call function HERE
+                return #?
+            elif prompt not in [1, 2, 3]:
+                print("Invalid input. Please try again.")
+
+
+
+blacksmith_items = [{"name": "Helmet", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 25)), "price": (random.randint((adventurer.ranking + 10), (adventurer.ranking + 15)) * 2)}
+    ,{"name": "Chestplate", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 20)), "price": (random.randint((adventurer.ranking + 15), (adventurer.ranking + 20)) * 2)}
+    ,{"name": "Leggings", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 25)), "price": (random.randint((adventurer.ranking + 10), (adventurer.ranking + 15)) * 2)}
+    ,{"name": "Boots", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 20)), "price": (random.randint((adventurer.ranking + 5), (adventurer.ranking + 10)) * 2)}
+    ,{"name": "Sword", "boost": random.randint((adventurer.ranking), (adventurer.ranking + 30)), "price": (random.randint((adventurer.ranking + 15), (adventurer.ranking + 20)) * 2)}]
 blacksmith = shop("blacksmith", blacksmith_items)
 blacksmith.prepare_shop()
