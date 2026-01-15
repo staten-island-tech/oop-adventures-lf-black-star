cd221991755d4eba8767ea2c9485bc4fbaff3436
Vincent <vincentz92@nycstudents.net>
2026-01-14 19:12:32 -0500

    a

--- equipment.py
+++ equipment.py
@@ -7,8 +7,8 @@ class Equipment:
         self.stat_boost = stat_boost
 
 hermes = Equipment("Blessing of Hermes", adventurer.speed, 1)
-aphrodite = Equipment("Blessing of Aphrodite", adventurer.regen, (adventurer.health/10))
+aphrodite = Equipment("Blessing of Aphrodite", adventurer.regen, round(adventurer.health/10))
 plutus = Equipment("Blessing of Plutus", adventurer.wealth, 1)
 athena = Equipment("Blessing of Athena", adventurer.weapon, round(adventurer.weapon/5))
 ares = Equipment("Blessing of Ares", adventurer.damage, round(adventurer.damage/2))
-relic_list = [hermes, aphrodite, ]
+relic_list = [hermes, aphrodite, plutus, athena, ares]
--- hero.py
+++ hero.py
@@ -1,10 +1,16 @@
+from equipment import hermes
+from equipment import aphrodite
+from equipment import plutus
+from equipment import athena
+from equipment import ares
+#TEMPORARY
 class Hero:
-    def __init__(self, health, damage, inventory, tokens, relics, speed, regen, wealth, weapon, helmet, chestplate, leggings, boots, ranking):
+    def __init__(self, health, damage, inventory, tokens, obtained_relics, speed, regen, wealth, weapon, helmet, chestplate, leggings, boots, ranking):
         self.health = health
         self.damage = damage
         self.inventory = inventory
         self.tokens = tokens
-        self.relics = relics
+        self.obtained_relics = obtained_relics
         self.speed = speed
         self.regen = regen
         self.wealth = wealth
@@ -16,7 +22,16 @@ class Hero:
         self.ranking = ranking
 
     def apply_boosts(self):
-
+        if hermes in self.obtained_relics:
+            self.speed += hermes.stat_boost
+        if aphrodite in self.obtained_relics:
+            self.regen += aphrodite.stat_boost
+        if plutus in self.obtained_relics:
+            self.wealth += plutus.stat_boost
+        if athena in self.obtained_relics:
+            self.weapon += athena.stat_boost
+        if ares in self.obtained_relics:
+            self.damage += ares.stat_boost
         self.health += (self.chestplate + self.helmet + self.leggings + self.boots)
         self.damage += self.weapon
     
